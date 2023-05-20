69d10cea57ed018f63ca8e8e8dc3b6e0e54379bd
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-20 16:20:08 +0900

    Update stream.app.py
    added breatfast menu header and 3 items

--- stream.app.py
+++ stream.app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('test')
+streamlit.title("My Mom's New Healthy Diner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text(🥗'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🐔'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🥑🍞'Hard-Boiled Free-Range Egg')
