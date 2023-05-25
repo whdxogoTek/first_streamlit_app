fe58d9ab80a1449b214c2f4a1a14e1e048bd7056
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-26 03:46:56 +0900

    Update stream.app.py

--- stream.app.py
+++ stream.app.py
@@ -36,7 +36,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 import snowflake.connector 
-
+#
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
