fd15fc7d37114d244f876f00b805c5a31cae08a2
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-28 15:45:07 +0900

    Update stream.app.py

--- stream.app.py
+++ stream.app.py
@@ -47,15 +47,15 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 # --- basic 
-my_cur.exexute("select * from fruit_load_list")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my.cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
 # --- make it nicer 
-my_data_row = my_cur.fetchone()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+# my_data_row = my_cur.fetchone()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_row)
 
 
 # --- Get All the Rows 
