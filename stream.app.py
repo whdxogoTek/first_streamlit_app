5e574f3d91b3f4b7b5f5b98240eccfb39db020fc
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-28 17:17:39 +0900

    Update stream.app.py

--- stream.app.py
+++ stream.app.py
@@ -41,18 +41,37 @@ streamlit.dataframe(fruits_to_show)
 
 # ------------------ New Code ---------------------------------------------------------
 # New Section 
+# streamlit.header("Fruityvice Fruit Advice!")
+# try:  
+#    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#    if not fruit_choice:
+#        streamlit.error("Please select a fruit to get information.")
+#    else:
+#            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
+#            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#            streamlit.dataframe(fruityvice_normalized)
+#    except URLError as e:
+#    streamlit.error()
+  
+
+import streamlit
+import requests
+import pandas
+from urllib.error import URLError
+
 streamlit.header("Fruityvice Fruit Advice!")
+
 try:  
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
-            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-            streamlit.dataframe(fruityvice_normalized)
-    except URLError as e:
-    streamlit.error()
-  
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error("An error occurred while retrieving the fruit information.")
+
 # -------------------------------------------------------------------------------------------    
     
     
