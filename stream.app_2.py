a1ed082e422d3196b18595d35b7dbf84a0fb1e79
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-28 23:55:04 +0900

    Update stream.app_2.py

--- stream.app_2.py
+++ stream.app_2.py
@@ -79,10 +79,10 @@ my_data_rows = my_cur.fetchall() # 한 번에 모든 로우를 읽기 위해서�
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-# ▶▶▶▶▶▶▶▶▶▶ Lesson 12-[]
+# ▶▶▶▶▶▶▶▶▶▶ Lesson 12-[] (BEFORE 🥋 이제 과일 이름 제출을 추가하기 위해 기능과 버튼을 사용하자)
 # --------------------- 🎯 Can You Add A Second Text Entry Box?  ----------------------------------
-add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit')
-streamlit.write('Theanks for adding jackfruit', add_my_fruit)
+# add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit')
+# streamlit.write('Theanks for adding jackfruit', add_my_fruit)
 
 # ▶▶▶▶▶▶▶▶▶▶ Lesson 12-[] 🥋 Time to Tidy Up? 
 # -----------------------🥋 Write Code to Add Rows to Our Fruit List in Snowflake-------------------------
@@ -92,15 +92,16 @@ streamlit.write('Theanks for adding jackfruit', add_my_fruit)
 
 streamlit.write('Thanks for adding', add_my_fruit)
 # This will not work correctly, but just go with it for now 
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+# my_cur.execute("insert into fruit_load_list values('from streamlit')") # -- BEFORE 
 
 # -------------------------🥋 Add a STOP Command to Focus Our Attention---------------------------------------
+# -------------------------- BEFORE -------------------------------------------------------
 # streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
 
 # -------------------------- 🥋 재구성된 버전을 실행하고 여전히 작동하는지 확인 -----------------------------------
 # streamlit 가서 확인중... 
@@ -127,6 +128,21 @@ streamlit.text(my_data_row)
 # ▶▶▶▶▶▶▶▶▶▶ Lesson 12-[] AFTER 
 # ------------------------------🥋 함수 만들기  ------------------------------------------
 # Create the called a function 
+# def get_fruityvice_data(this_fruit_choice):
+#       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) 
+#       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#       return fruityvice_normalized
+
+# # New Section 
+# streamlit.header("Fruityvice Fruit Advice!")
+# try:  
+#     fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#     if not fruit_choice:
+#         streamlit.error("Please select a fruit to get information.")
+#     else:   
+#           back_from_function = get_fruityvice_data(fruit_choice)
+#           streamlit.dataframe(back_from_function)   
+
 def get_fruityvice_data(this_fruit_choice):
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) 
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -139,5 +155,42 @@ try:
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:   
-          back_from_function = get_fruityvice_data(fruit_choice)
-          streamlit.dataframe(back_from_function)        
+            back_from_function = get_fruityvice_data(fruit_choice)
+            streamlit.dataframe(back_from_function) 
+except URLError as e:
+    streamlit.error("An error occurred while retrieving the fruit information.")
+
+
+
+# ▶▶▶▶▶▶▶▶▶▶ Lesson 12-[] AFTER 
+# ------------------- 🥋 과일 로드 목록 쿼리를 이동하고 버튼 동작으로 로드 --------------------
+streamlit.header("The fruit load list contains:")
+# Snowflake-related funtions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur: 
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall() 
+
+# Add a button to load the fruit 
+if streamlit.button("Get Fruit Load List"): 
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
+
+# ------------------------------ 🥋 이제 과일 이름 제출을 추가하기 위해 기능과 버튼을 사용하자 -----------------------
+streamlit.header("The fruit load list contains:")
+# Snowflake-related funtions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur: 
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall() 
+
+# Add a button to load the fruit 
+if streamlit.button("Get Fruit Load List"): 
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
