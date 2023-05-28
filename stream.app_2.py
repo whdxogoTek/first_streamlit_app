f47ee1591901c107d1bac3bb404dc5d501db0f07
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-28 23:57:23 +0900

    Update stream.app_2.py

--- stream.app_2.py
+++ stream.app_2.py
@@ -90,7 +90,7 @@ streamlit.dataframe(my_data_rows)
 # --- select * 
 # --- from fruit_load_list ; 
 
-streamlit.write('Thanks for adding', add_my_fruit)
+# streamlit.write('Thanks for adding', add_my_fruit)
 # This will not work correctly, but just go with it for now 
 # my_cur.execute("insert into fruit_load_list values('from streamlit')") # -- BEFORE 
 
