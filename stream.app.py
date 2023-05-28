6372d19f1e0721cd81109139e391abd0460673cf
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-28 15:42:10 +0900

    Update stream.app.py

--- stream.app.py
+++ stream.app.py
@@ -59,10 +59,10 @@ streamlit.dataframe(my_data_row)
 
 
 # --- Get All the Rows 
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows) 
+# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_rows) 
 
 
 
