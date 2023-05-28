760f2404d1ef0c95cb05e95c53fa88e1149de023
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-28 18:05:44 +0900

    Update stream.app.py

--- stream.app.py
+++ stream.app.py
@@ -93,42 +93,59 @@ except URLError as e:
     streamlit.error("An error occurred while retrieving the fruit information.")
     
     
+# ----------------------------------code_2 (button) -------------------------------------------------------
+streamlit.header("The fruit load list contains:")
+# Snowflake-related funtions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur: 
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall() 
 
-# import streamlit
-# import pandas as pd
-# import snowflake.connector
+# Add a button to load the fruit 
+if streamlit.button("Get Fruit Load List'): 
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.dataframe(my_data_row)
 
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
-# --- basic 
-# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-# my_data_row = my.cur.fetchone()
-# streamlit.text("The fruit load list contains:")
+
+
+
+
+
+
+# # -----------------------------------------code_1-----------------------------------------------------------------
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
 # streamlit.text(my_data_row)
 
-# --- make it nicer 
-# my_data_row = my_cur.fetchone()
+# # --- basic 
+# # my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+# # my_data_row = my.cur.fetchone()
+# # streamlit.text("The fruit load list contains:")
+# # streamlit.text(my_data_row)
+
+# # --- make it nicer 
+# # my_data_row = my_cur.fetchone()
+# # streamlit.header("The fruit load list contains:")
+# # streamlit.dataframe(my_data_row)
+
+
+# # --- Get All the Rows 
+# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+# my_data_rows = my_cur.fetchall()
 # streamlit.header("The fruit load list contains:")
-# streamlit.dataframe(my_data_row)
+# streamlit.dataframe(my_data_rows) 
 
+# # --- Challenge_1 
+# add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+# streamlit.write('Theanks for adding jackfruit', add_my_fruit)
 
-# --- Get All the Rows 
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows) 
-
-# --- Chagggenge 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('Theanks for adding jackfruit', add_my_fruit)
-
-
+#---------------------------------------------------------------------------------------------------------
 
 
