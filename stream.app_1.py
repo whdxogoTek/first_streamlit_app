f886c94d11b6805e3f8ce086afcd1d0d6487579d
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-28 21:33:18 +0900

    Update stream.app_1.py

--- stream.app_1.py
+++ stream.app_1.py
@@ -46,3 +46,18 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected] ## loc[:,'fruits_selected'] 
 streamlit.dataframe(fruits_to_show)
+
+# ▶▶▶▶▶▶▶▶▶▶ Lesson 7-[]
+# ----------------- 🥋 Same common ------------------------------------------------------
+
+streamlit.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = request.get('https://fruityvice.com/api/fruit/" + "fruit_choice")
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
