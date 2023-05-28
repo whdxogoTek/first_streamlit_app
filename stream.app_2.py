840a8500fffb661643fd3e212f55423488e31825
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-28 23:08:09 +0900

    Update stream.app_2.py

--- stream.app_2.py
+++ stream.app_2.py
@@ -107,3 +107,15 @@ streamlit.text(my_data_row)
 
 # ▶▶▶▶▶▶▶▶▶▶ Lesson 12-[] 
 # ------------------------------ 🥋 Fruityvice 코드를 Try-Except로 이동(중첩된 If-Else 포함) ---------------------
+# New Section 
+streamlit.header("Fruityvice Fruit Advice!")
+try:  
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+       streamlit.error("Please select a fruit to get information.")
+   else:
+           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
+           fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+           streamlit.dataframe(fruityvice_normalized)
+   except URLError as e:
+   streamlit.error()
