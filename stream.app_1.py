bdfdcdeab5681d67e2eb6c2ad9ce2e46c380af61
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-05-28 21:38:54 +0900

    Update stream.app_1.py

--- stream.app_1.py
+++ stream.app_1.py
@@ -50,14 +50,21 @@ streamlit.dataframe(fruits_to_show)
 # ▶▶▶▶▶▶▶▶▶▶ Lesson 7-[]
 # ----------------- 🥋 Same common ------------------------------------------------------
 
-streamlit.header('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+# streamlit.header('Fruityvice Fruit Advice')
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# streamlit.write('The user entered ', fruit_choice)
+
+# fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# # write your own comment -what does the next line do? 
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# # write your own comment - what does this do?
+# streamlit.dataframe(fruityvice_normalized)
+
+
 
