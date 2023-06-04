ff4b01d8b45111e76710f66d8dbbeb63ffedd64c
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-06-04 20:44:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -13,34 +13,34 @@ import random
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
 
-#-----------------------------------------------------------------------------------
+# #-----------------------------------------------------------------------------------
 
-streamlit.header("The Fruity List Contains:")
-def get_test_food():
-  with my_cnx.cursor() as my_cur:
-      my_cur.execute("select * from test_food ")
-      return my_cur.fetchall()
+# streamlit.header("The Fruity List Contains:")
+# def get_test_food():
+#   with my_cnx.cursor() as my_cur:
+#       my_cur.execute("select * from test_food ")
+#       return my_cur.fetchall()
     
-def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur:
-      my_cur.execute("insert into test_food values ('" + new_fruit + "' )")
-      return "Thanks for adding " + new_fruit
+# def insert_row_snowflake(new_fruit):
+#   with my_cnx.cursor() as my_cur:
+#       my_cur.execute("insert into test_food values ('" + new_fruit + "' )")
+#       return "Thanks for adding " + new_fruit
      
-add_my_fruit = streamlit.text_input('What food would you like to add?')
+# add_my_fruit = streamlit.text_input('What food would you like to add?')
 
-if streamlit.button('Get food List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  streamlit.write(insert_row_snowflake(add_my_fruit))
-  my_data_rows = get_test_food()
-  my_cnx.close()
-  streamlit.dataframe(my_data_rows)
+# if streamlit.button('Get food List'):
+#   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#   streamlit.write(insert_row_snowflake(add_my_fruit))
+#   my_data_rows = get_test_food()
+#   my_cnx.close()
+#   streamlit.dataframe(my_data_rows)
   
   
   
@@ -55,6 +55,38 @@ if streamlit.button('Get food List'):
   
 # get_test_food(my_data_rows)
 
+import random
+import streamlit as streamlit
+
+streamlit.header("The Fruity List Contains:")
+
+def get_test_food():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM test_food")
+        return my_cur.fetchall()
+
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("INSERT INTO test_food VALUES ('" + new_fruit + "' )")
+    return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What food would you like to add?')
+
+if streamlit.button('Get food List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    streamlit.write(insert_row_snowflake(add_my_fruit))
+    my_data_rows = get_test_food()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
+
+if streamlit.button('Pick a Random Fruit'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_test_food()
+    my_cnx.close()
+    
+    fruits = [row[0] for row in my_data_rows]
+    random_fruit = random.choice(fruits)
+    streamlit.write("Randomly picked fruit:", random_fruit)
 
 
 
