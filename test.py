600a4f9acf91ebfb1085321afedd81e0f1e54b38
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-06-06 15:36:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -61,7 +61,7 @@ import streamlit as streamlit
 streamlit.title("식사를 합시다. 😎")
 
 streamlit.header('점심 매뉴도 똑똑하게! 우리는 Proven Bees!')
-streamlit.text('🥗 더 이상 고민하는 점심매뉴 그만!'
+streamlit.text('🥗 더 이상 고민하는 점심매뉴 그만!')
 streamlit.text('🎯 즐겁고 빠르게 점심식사 매뉴를 정해봐요!')
 
                
