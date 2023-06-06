0b7152fb4a14a890dd9d14393262807161916545
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-06-06 15:44:10 +0900

    Update test.py

--- test.py
+++ test.py
@@ -60,14 +60,12 @@ import streamlit as streamlit
 
 streamlit.title("식사를 합시다. 😎")
 
-streamlit.header('점심 매뉴도 똑똑하게! 우리는 Proven Bees!')
+## streamlit.header('점심 매뉴도 똑똑하게! 우리는 Proven Bees!')
+streamlit.markdown('#### 점심 매뉴도 똑똑하게! 우리는 Proven Bees!')    
 streamlit.text('🥗 더 이상 고민하는 점심매뉴 그만!')
-streamlit.text('🎯 즐겁고 빠르게 점심식사 매뉴를 정해봐요!')
-streamlit.markdown('### 🥗No more contemplating lunch menu!')
-streamlit.markdown('#### 🥗No more contemplating lunch menu!')       
+streamlit.text('🎯 즐겁고 빠르게 점심식사 매뉴를 정해봐요!')     
 
-
-streamlit.header("점심식사 매뉴 추가하기:")               
+streamlit.markdown('#### 점심식사 매뉴 추가하기:')              
 
 def get_test_food():
     with my_cnx.cursor() as my_cur:
