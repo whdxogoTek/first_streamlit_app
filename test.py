22fab1099e724eea7b2e7b8b93475c6c2351b816
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-06-06 16:15:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -97,9 +97,9 @@ if streamlit.button('Pick a Random Fruit'):
     
        
 streamlit.markdown('#### 점심식사 Dataframe:')         
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_data_rows = get_test_food()
-my_cnx.close()
+# my_cnx.close()
 my_food_list = streamlit.dataframe(my_data_rows)
 my_food_list = my_food_list.set_index('FOOD_LIST')
 my_food_list_selected = streamlit.multiselect("Pick 식당:", list(my_food_list.index), ['기소야', '동경규동'])
