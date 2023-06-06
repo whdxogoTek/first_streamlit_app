23055edfbd102221969aec180bbaee21c55272de
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-06-06 16:47:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -115,7 +115,6 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM test_food")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 
