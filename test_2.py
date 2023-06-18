5be20eb7bec4c85ea1ce20d75855de009e7ae926
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-06-18 15:39:48 +0900

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -15,3 +15,18 @@ streamlit.text('🎯 즐겁고 빠르게 점심식당을 정해봐요!')
 
 streamlit.markdown('#### 점심식당 추가하기:')      
 streamlit.text('📝 식당을 추가해봅시다.')  
+
+------------------------------------------------------------
+def get_test_food():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM FACT_RESTAURANT")
+        return my_cur.fetchall()
+    
+def insert_row_snowflake(new_RESTAURANT):
+    if not new_RESTAURANT:
+        return "식당을 입력하세요!"
+    
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("INSERT INTO test_food VALUES ('" +new_RESTAURANT+ "' )")
+    
+    return "식당을 추가했습니다~🤗" 
