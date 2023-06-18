8c501be17fc4c9f7d02980d816c3e7de09819d0a
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-06-18 19:19:29 +0900

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -17,30 +17,60 @@ streamlit.markdown('#### 점심식당 추가하기:')
 streamlit.text('📝 식당을 추가해봅시다.')  
 
 # ------------------------------------------------------------
-def get_test_RESTAURANT():
+def get_test_food():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * FROM FACT_RESTAURANT")
         return my_cur.fetchall()
-    
-def insert_row_snowflake(new_RESTAURANT):
-    #if not new_RESTAURANT:
-    #    return "식당을 입력하세요!"
+       
+def insert_row_snowflake(new_fruit):
+    if not new_fruit:
+        return "Enter fruits"
     
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("INSERT INTO FACT_RESTAURANT VALUES ('" + new_RESTAURANT + "' )")
+        my_cur.execute("INSERT INTO FACT_RESTAURANT VALUES ('" + new_fruit + "' )")
     
-    return "Thanks for adding " + new_RESTAURANT
+    return "Thanks for adding " + new_fruit
+add_my_fruit = streamlit.text_input('What food would you like to add?')
 
-# -----------------------------------------------------------------
-Data_Restaurant_ID = streamlit.text_input('식당을 추가하세요')
-Data_Restaurant_Type = streamlit.text_input('식당 종류를 작성해주세요')
-Data_Restaurant_Add_Name = streamlit.text_input('식당을 추가한 당신의 성함을 작성해주세요')
-
-if streamlit.button('식당 데이터 확인 ✅'):
+if streamlit.button('Get food List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    streamlit.write(insert_row_snowflake(Data_Restaurant_ID))
-    # streamlit.write(insert_row_snowflake(Data_Restaurant_Type))
-    # streamlit.write(insert_row_snowflake(Data_Restaurant_Add_Name))
-    my_data_rows = get_test_RESTAURANT()
+    streamlit.write(insert_row_snowflake(add_my_fruit))
+    my_data_rows = get_test_food()
     my_cnx.close()
     streamlit.dataframe(my_data_rows)
+
+
+
+
+
+
+
+
+
+# def get_test_RESTAURANT():
+#     with my_cnx.cursor() as my_cur:
+#         my_cur.execute("SELECT * FROM FACT_RESTAURANT")
+#         return my_cur.fetchall()
+    
+# def insert_row_snowflake(new_RESTAURANT):
+#     #if not new_RESTAURANT:
+#     #    return "식당을 입력하세요!"
+    
+#     with my_cnx.cursor() as my_cur:
+#         my_cur.execute("INSERT INTO FACT_RESTAURANT VALUES ('" + new_RESTAURANT + "' )")
+    
+#     return "Thanks for adding " + new_RESTAURANT
+
+# # -----------------------------------------------------------------
+# Data_Restaurant_ID = streamlit.text_input('식당을 추가하세요')
+# Data_Restaurant_Type = streamlit.text_input('식당 종류를 작성해주세요')
+# Data_Restaurant_Add_Name = streamlit.text_input('식당을 추가한 당신의 성함을 작성해주세요')
+
+# if streamlit.button('식당 데이터 확인 ✅'):
+#     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#     streamlit.write(insert_row_snowflake(Data_Restaurant_ID))
+#     # streamlit.write(insert_row_snowflake(Data_Restaurant_Type))
+#     # streamlit.write(insert_row_snowflake(Data_Restaurant_Add_Name))
+#     my_data_rows = get_test_RESTAURANT()
+#     my_cnx.close()
+#     streamlit.dataframe(my_data_rows)
