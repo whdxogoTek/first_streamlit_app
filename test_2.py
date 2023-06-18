88a81a9b9916c54de0d0d8438be18f94493cf62a
whdxogoTek <96727467+whdxogoTek@users.noreply.github.com>
2023-06-18 15:34:08 +0900

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,1 +1,16 @@
 
+import streamlit 
+import pandas
+import requests
+import snowflake.connector 
+from urllib.error import URLError 
+import seaborn
+import numpy
+import random
+
+streamlit.markdown('#### 점심식당 정하기!')    
+streamlit.text('🥗 더 이상 어디갈지 고민은 그만!')
+streamlit.text('🎯 즐겁고 빠르게 점심식당을 정해봐요!')     
+
+streamlit.markdown('#### 점심식당 추가하기:')      
+streamlit.text('📝 식당을 추가해봅시다.')  
